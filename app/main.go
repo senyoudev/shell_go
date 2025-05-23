79ab07789ddb607e7d042845f1e267b2cc48141d
senyoudev <younesmeskafe2018@gmail.com>
2025-05-24 00:50:48 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -15,7 +15,8 @@ func main() {
 	// Wait for user input
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
-		fmt.Errorf("Error reading the command")
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
 	}
 
 	fmt.Println(command[:len(command)-1] + ": command not found")
