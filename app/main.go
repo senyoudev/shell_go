3ba683a567501f7030156786c3e32bc165b06559
senyoudev <younesmeskafe2018@gmail.com>
2025-05-24 00:49:46 +0100

    [any message]

--- app/main.go
+++ app/main.go
@@ -10,9 +10,14 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Errorf("Error reading the command")
+	}
+
+	fmt.Println(command[:len(command)-1] + ": command not found")
+
 }
