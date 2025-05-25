59cb2880f6122e561111dc0e5fdb73922ca8f4de
senyoudev <younesmeskafe2018@gmail.com>
2025-05-25 17:07:54 +0100

    codecrafters submit [skip ci]

--- app/internal/commands/CdCommand.go
+++ app/internal/commands/CdCommand.go
@@ -3,16 +3,26 @@ package commands
 import (
 	"fmt"
 	"os"
+	"strings"
+	"path"
 )
 
 
 type CdCommand struct{}
 func (c CdCommand) Name() string { return "cd" }
 func (c CdCommand) Execute(args []string) error {
-	path := args[0]
-	err := os.Chdir(path)
+	path_arg := args[0]
+	if strings.HasPrefix(path_arg, "~") {
+		HOME := os.Getenv("HOME")
+		if (len(HOME)) == 0 {
+			fmt.Println("cd: $HOME is not set.")
+		} else {
+			path_arg = path.Join(HOME, path_arg[1:])
+		}
+	}
+	err := os.Chdir(path_arg)
 	if err != nil {
-		fmt.Printf("%s: %s: No such file or directory\n", c.Name(), path)
+		fmt.Printf("%s: %s: No such file or directory\n", c.Name(), path_arg)
 	}
 	return nil
 }
